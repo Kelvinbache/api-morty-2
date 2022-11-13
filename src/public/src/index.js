3213a6129370e980c58d528c8e28b004ed09e702
KELVIN <kelvinabache@gmail.com>
2022-11-13 09:42:59 -0500

    usando una api de datos

--- src/public/src/index.js
+++ src/public/src/index.js
@@ -1,12 +1,36 @@
+'use strict';
+
  async function llamadaDeApi () {
 
    const api = await fetch('https://jsonplaceholder.typicode.com/users/1/todos');
-
    const apiJson = await api.json();
-   const array = apiJson;
-  array.forEach(elementos => console.log(elementos););
 
+   const array =  apiJson;
 
+    const reformar = array.map((objectos) => {
+        const valores =  [];
+        valores[objectos.id] = objectos.title;
+        return valores.forEach(creadora);
+    } );
  }
 
-console.log(llamadaDeApi());
+function creadora (titulos,id){
+const cuerpoDeTabla = document.getElementById('cuerpoDeLaTabala');
+const tr = document.createElement('tr');
+
+  for (let  i = 0; i < 1 ; i++) {
+     const td1 = document.createElement('td');
+     const td2 = document.createElement('td');
+
+   td2.setAttribute('class','table-active');
+
+     td1.textContent = id;
+     td2.textContent = titulos;
+
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+  }
+    cuerpoDeTabla.appendChild(tr);
+}
+
+llamadaDeApi();
